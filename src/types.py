d01a84bdc4d3a78fb0a0bb123f688abca074024e
nickavdeev <mail@nickavdeev.me>
2024-08-16 17:09:25 +0200

    Typing fix

--- src/types.py
+++ src/types.py
@@ -1,6 +1,6 @@
 from typing import NewType
 
 
-type UserId = NewType("UserId", int)
-type UserLanguage = NewType("UserLanguage", str)
-type WordMeaning = tuple[bool, str]
+UserId = NewType("UserId", int)
+UserLanguage = NewType("UserLanguage", str)
+WordMeaning = tuple[bool, str]
