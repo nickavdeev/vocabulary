d87e86ce2b22174d2df630806985076c6bc30587
nickavdeev <mail@avdeev.me>
2025-08-13 22:48:41 +0200

    Rollback with using only one language

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,15 @@
 ADD_TO_VOCABULARY_CALLBACK = "add_to_vocabulary"
 
-ADDED_TO_VOCABULARY = "✔️ Added to vocabulary"
-WORD_IN_VOCABULARY = "✔️ <i>This word is already in your vocabulary</i>"
+ADDED_TO_VOCABULARY_TEXT = "✔️ Added to vocabulary"
+WORD_IN_VOCABULARY_TEXT = "✔️ <i>This word is already in your vocabulary</i>"
 
 WELCOME_MESSAGE = (
     "👋 <b>Welcome to the Vocabulary Bot!</b>\n\n"
-    "Here you can learn new words of a foreign language.\n\n"
-    "Use the following commands to interact with the bot:\n"
-    "/language - change the language you want to learn\n"
-    "/vocabulary - show your vocabulary"
+    "Send me an English word, and I’ll give you its definitions. "
+    "Add it to your list and review it later — I’ll remind you to "
+    "practice based on the forgetting curve.\n\n"
+    "Use these commands to interact with the bot:\n"
+    "/vocabulary — view your vocabulary list"
 )
 
 DAYS_BY_PHASES = {
@@ -21,12 +22,13 @@ DAYS_BY_PHASES = {
     6: 0,
 }
 
-VISIBLE_LANGUAGES = {
-    "en": "🇬🇧 English",
-    "de": "🇩🇪 German",
-}
-
 # Button names
 
-BUTTON_VOCABULARY = "📚 Vocabulary"
-BUTTON_LANGUAGE = "🌍 Language"
+VOCABULARY_BUTTON = "📖 Vocabulary"
+
+# Texts
+
+EMPTY_VOCABULARY_TEXT = (
+    "🔎 <b>Your vocabulary is empty</b>, but you can expand it: "
+    "just send me an English word, and I’ll give you its definitions."
+)
